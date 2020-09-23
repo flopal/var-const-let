9d546595978115577fcf3fb129b71512fed03752
flopal <florent.palanque@hotmail.fr>
2020-09-23 14:58:15 +0000

    Don't need scope directory - display all file js in a hell organization - add variable-change-value.js

---
+++ variable-change-value.js
@@ -0,0 +1,26 @@
+const var_const = 1;
+let var_let = 1;
+var var_var = 1;
+console.log('Var with const statement =', var_const);
+console.log('Var with let statement =', var_let);
+console.log('Var with var statement =', var_var);
+
+// Uncomment the line below and you will have an error
+// const var_const = 2;
+
+// Uncomment the line below and you will have an error
+// let var_let = 2;
+
+// Please comment the line below. I prefer to see all variable to 1.
+var var_var = 2;
+console.log('Var with const statement =', var_const);
+console.log('Var with let statement =', var_let);
+console.log('Var with var statement =', var_var);
+
+// Uncomment the line below and you will have an error. It's a constant.
+// var_const = 2;
+var_let = 2;
+var_var = 2;
+console.log('Var with const statement =', var_const);
+console.log('Var with let statement =', var_let);
+console.log('Var with var statement =', var_var);
